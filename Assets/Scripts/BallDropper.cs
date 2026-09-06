2e91fe27895b92f4257fed8b6ad9e3cf76659957
maxvmorozov <maxvmorozov@gmail.com>
2026-09-06 16:58:41 -0700

    made balls launch in random directions

--- Assets/Scripts/BallDropper.cs
+++ Assets/Scripts/BallDropper.cs
@@ -14,11 +14,21 @@ public class BallDropper : MonoBehaviour
         }
     }
 
-    public void DropBall()
+    private void DropBall()
     {
         Vector3 spawnPosition = GetSpawnPosition();
         
-        Instantiate(BallPrefab, spawnPosition, Quaternion.identity);
+       GameObject ball = Instantiate(BallPrefab, spawnPosition, Quaternion.identity);
+
+        AddRandomForce(ball);
+    }
+
+    private void AddRandomForce(GameObject ball)
+    {
+        Rigidbody2D rigidbody = ball.GetComponent<Rigidbody2D>();
+        
+        float randomHorizontalForce = Random.Range(-5.5f, 5.5f);
+        rigidbody.AddForce(new Vector2(randomHorizontalForce, 0f), ForceMode2D.Impulse);
     }
 
     private Vector3 GetSpawnPosition()
